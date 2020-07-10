b3ca5aa91292075055b77fca9486f39b33013ded
Nicholas Chua <nmchua8@gmail.com>
2020-07-10 21:15:14 +1000

    Connect to MongoDB Database

--- src/orders.js
+++ src/orders.js
@@ -1,25 +1,21 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
-// Initalising global variables for collections
-let customers;
-let companies;
-let deliveries;
-let orderItems;
-let orders;
-
 // Connecting to database
-const url = "mongodb://localhost:27017/";
+const url = "mongodb://localhost:27017";
 async function connect() {
   const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
+  
   try {
     await client.connect();
+
+    // Accessing items in database 
     const db = client.db("packform_coding_test");
-    customers = db.collection('customers').find({});
-    companies = db.collection('companies').find({});
-    deliveries = db.collection('deliveries').find({});
-    orderItems = db.collection('orderItems').find({});
-    orders = db.collection('orders').find({});
+    customers = db.collection("customers").find({}).toArray();
+    companies = db.collection("companies").find({}).toArray();
+    deliveries = db.collection("deliveries").find({}).toArray();
+    orderItems = db.collection("orderItems").find({}).toArray();
+    orders = db.collection("orders").find({}).toArray();
   }
   catch (ex) {
     console.error('Something bad happened. '+ex);
@@ -29,4 +25,4 @@ async function connect() {
   }
 }
 
-connect()
+connect();
